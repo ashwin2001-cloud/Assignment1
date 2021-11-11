98be739ff5805d6df7c614137c7db3fc49fb5d94
Ashwin Baranwal <ashwinbaranwal2001@gmail.com>
2021-11-11 18:57:04 +0530

    deploy

--- app.js
+++
@@ -1,6 +0,0 @@
-function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-
-console.log(isValidEmail("a@gmail.com"));
